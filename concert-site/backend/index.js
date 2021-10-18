9bd922e3afd5895218cf4fde91c3a96866ca5515
Cole Milne <colemilne54@gmail.com>
2021-10-18 14:19:50 -0600

    good

--- concert-site/backend/index.js
+++ concert-site/backend/index.js
@@ -7,19 +7,18 @@ const MongoClient = mongodb.MongoClient
 const port = process.env.port || 8000
 
 MongoClient.connect(
-    proccess.env.CONCERTS_DB_URI,
+    process.env.CONCERTS_DB_URI,
     {
-        poolSize: 20,
-        wtimout: 2500,
-        useNewUrlParse: true 
-    }
-)
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-})
-.then(async client => {
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`)
+        maxPoolSize: 20,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true }
+    )
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
     })
-})
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`)
+        })
+    })
