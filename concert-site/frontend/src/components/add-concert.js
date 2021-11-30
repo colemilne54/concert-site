7b5ec8c84d7fd2ed6d8057ba2047f58557505193
Cole Milne <colemilne54@gmail.com>
2021-11-30 13:04:41 -0700

    dont think this will work

--- concert-site/frontend/src/components/add-concert.js
+++ concert-site/frontend/src/components/add-concert.js
@@ -1,132 +1,33 @@
-import React, { useState } from "react";
-import ConcertDataService from "../services/concert";
-import { Link } from "react-router-dom";
-import '../style.css';
+import React, { useEffect, useState } from 'react'
 
-// const Concert = props => {
-//   const initialConcertState = {
-//     id: null,
-//     user_name: "",
-//     user_id: "",
-//     bands: "",
-//     street: "",
-//     zipcode: "",
-//     venueType: "",
-//     genre: "",
-//     venue_name: "",
-//     date: "",
-//     link: "",
-//     state: "",
-//     city: ""
-//   };
-//   const [concert, setConcert] = useState(initialConcertState);
+const Contact = () => {
 
-const AddConcert = props => {
-  let initialConcertState = {
-    id: null,
-    user_name: "",
-    user_id: "",
-    bands: "",
-    street: "",
-    zipcode: "",
-    venueType: "",
-    genre: "",
-    venue_name: "",
-    date: "",
-    link: "",
-    state: "",
-    city: ""
-  };
+    const [concertData, setConcertData] = useState({});
 
-  let editing = false;
+    const concertData = async() => {
+        try {
+            const res = await fetch('/', {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
 
-  // figure out the props.etc.etc
-  if (props.location.state && props.location.state.currentConcert) {
-    editing = true;
-    initialConcertState = props.location.state.currentConcert.text
-  }
+            const data = await res.json();
+            console.log(data);
+            setConcertData(data);
 
-  const [concert, setConcert] = useState(initialConcertState);
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleInputChange = event => {
-    setConcert(event.target.value);
-  };
-
-  const saveConcert = () => {
-    // TODO: Change to list like 
-    var data = {
-      text: concert,
-      name: props.user.name,
-      user_id: props.user.id,
-      concert_id: props.match.params.id
-    };
-
-    if (editing) {
-      data.concert_id = props.location.state.currentConcert._id
-      ConcertDataService.updateConcert(data)
-        .then(response => {
-          setSubmitted(true);
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    } else {
-      ConcertDataService.createConcert(data)
-        .then(response => {
-          console.log(data.text);
-          setSubmitted(true);
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(data.text);
-          console.log(e);
-        });
+            if (!res.status === 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 
-  };
+    userEffect(() => {
+        userConcert();
+    }, []);
 
-  return (
-    <div>
-      {props.user ? (
-      <div className="submit-form">
-        {submitted ? (
-          <div>
-            <h4>You submitted successfully!</h4>
-            <Link to={"/"} className="btn btn-success">
-              Back to Concerts
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <div className="form-group">
-              <label htmlFor="description">{ editing ? "Edit" : "Create" } Concert</label>
-              <input
-                type="text"
-                className="form-control"
-                id="text"
-                required
-                value={concert}
-                onChange={handleInputChange}
-                name="text"
-              />
-            </div>
-            <button onClick={saveConcert} className="btn btn-success">
-              Submit
-            </button>
-          </div>
-        )}
-      </div>
-
-      ) : (
-      <div>
-        Add Concert
-      </div>
-      )}
-
-    </div>
-  );
-};
-
-export default AddConcert;
+}
