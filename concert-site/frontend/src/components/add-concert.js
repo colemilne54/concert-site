90e9c25dc7e91767e4474329a47976640b247cd3
Cole Milne <colemilne54@gmail.com>
2021-11-30 13:35:11 -0700

    commented out

--- concert-site/frontend/src/components/add-concert.js
+++ concert-site/frontend/src/components/add-concert.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-import addConcertImport from './imports/addConcertImport';
+// import addConcertImport from './imports/addConcertImport';
 
-export default function AddConcertForm() {
+// export default function AddConcertForm() {
 
-    const [bands, setBands] = useState("");
-    const [genre, setGenre] = useState("");
+//     const [bands, setBands] = useState("");
+//     const [genre, setGenre] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
 
-        const newConcert = {
-            bands,
-            genre
-        }
-        console.log(newConcert);
-    }
+//         const newConcert = {
+//             bands,
+//             genre
+//         }
+//         console.log(newConcert);
+//     }
 
-    let concertData = {
-        handleSubmit,
-        setBands,
-        setGenre
-    }
+//     let concertData = {
+//         handleSubmit,
+//         setBands,
+//         setGenre
+//     }
 
-    return (
-        <div>
-            <div>
-                <div>
-                    <addConcertImport concertState={concertData} />
-                </div>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <div>
+//                 <div>
+//                     <addConcertImport concertState={concertData} />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default AddConcertForm;
+// export default AddConcertForm;
 
