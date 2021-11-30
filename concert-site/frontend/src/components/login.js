125455fc03a2f6c783abf868cabe95986378e38c
cdanielson22 <b10f741@montana.edu>
2021-11-29 18:58:45 -0700

    Worked on login

--- concert-site/frontend/src/components/login.js
+++ concert-site/frontend/src/components/login.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
-//import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
+//import React from "react";
 
 
 const Login = props => {
