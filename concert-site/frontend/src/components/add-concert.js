90553bd429c268509f0dce56590fb1b079951b19
cdanielson22 <b10f741@montana.edu>
2021-11-30 17:05:16 -0700

    Worked on add review

--- concert-site/frontend/src/components/add-concert.js
+++ concert-site/frontend/src/components/add-concert.js
@@ -36,3 +36,106 @@
 
 // export default AddConcertForm;
 
+import React, { useState } from "react";
+import ConcertDataService from "../services/concert";
+import { Link } from "react-router-dom";
+
+const AddConcert = props => {
+    const initialConcertState = {
+      city: "",
+      state:"",
+      genre:"",
+      bands:"",
+      concert_id:"",
+      venue_name:"",
+      venueType:"",
+      street:"",
+      zipcode:"",
+      link:"",
+      date:""
+   };
+
+  
+
+  const [concert, setConcert] = useState(initialConcertState);
+  const [city, setCity] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = event => {
+    setConcert(event.target.value);
+  };
+
+  const saveConcert = () => {
+    var data = {
+      city: city
+
+      // need help here
+    };
+
+    // if (editing) {
+    //   data.review_id = props.location.state.currentReview._id
+    //   RestaurantDataService.updateReview(data)
+    //     .then(response => {
+    //       setSubmitted(true);
+    //       console.log(response.data);
+    //     })
+    //     .catch(e => {
+    //       console.log(e);
+    //     });
+    // } else {
+      ConcertDataService.createConcert(data)
+        .then(response => {
+          setSubmitted(true);
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    // }
+
+   };
+
+  return (
+    <div>
+      {props.user ? (
+      <div className="submit-form">
+        {submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <Link to={"/concerts-list/" + props.match.params.id} className="btn btn-success">
+              Back to Concerts
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="description">{ "Create" } City</label>
+              <input
+                type="text"
+                className="form-control"
+                id="text"
+                required
+                value={city}
+                onChange={handleInputChange}
+                name="text"
+              />
+            </div>
+            <button onClick={saveConcert} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+
+      ) : (
+      <div>
+        Please log in.
+      </div>
+      )}
+
+    </div>
+  );
+};
+
+export default AddConcert;
+
--- concert-site/frontend/src/components/login.js
+++ concert-site/frontend/src/components/login.js
@@ -1,71 +1,64 @@
 // // import logo from './logo.svg';
 // // import './App.css';
-// import React, { useState } from "react";
+import React, { useState } from "react";
 // //import React from "react";
 
 
-// const Login = props => {
+const Login = props => {
   
-//   const initialUserState = {
-//     name: "",
-//     userpin: "",
-//     id: ""
-//   };
+  const initialUserState = {
+    name: "",
+    userpin: "",
+    id: ""
+  };
 
-//   const [user, setUser] = useState(initialUserState);
+  const [user, setUser] = useState(initialUserState);
 
-//   const handleInputChange = event => {
-//     const { name, value } = event.target;
-//     setUser({ ...user, [name]: value });
-//   };
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
 
-//   const login = () => {
-//     props.login(user)
-//     props.history.push('/');
-//   }
+  const login = () => {
+    props.login(user)
+    props.history.push('/');
+  }
 
-//   return (
-//     <div className="submit-form">
-//       <div>
-//         <div className="form-group">
-//           <label htmlFor="user">Username</label>
-//           <input
-//             type="text"
-//             className="form-control"
-//             id="name"
-//             required
-//             value={user.name}
-//             onChange={handleInputChange}
-//             name="name"
-//           />
-//         </div>
+  return (
+    <div className="submit-form">
+      <div>
+        <div className="form-group">
+          <label htmlFor="user">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            required
+            value={user.name}
+            onChange={handleInputChange}
+            name="name"
+          />
+        </div>
 
-//         <div className="form-group">
-//           <label htmlFor="id">ID</label>
-//           <input
-//             type="text"
-//             className="form-control"
-//             id="id"
-//             required
-//             value={user.id}
-//             onChange={handleInputChange}
-//             name="id"
-//           />
-//         </div>
+        <div className="form-group">
+          <label htmlFor="id">ID</label>
+          <input
+            type="text"
+            className="form-control"
+            id="id"
+            required
+            value={user.id}
+            onChange={handleInputChange}
+            name="id"
+          />
+        </div>
 
-//         <button onClick={login} className="btn btn-success">
-//           Login
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-// /* function Login() {
-//   return (
-//     <div className="App">
-//       Login
-//     </div>
-//   );
-// }
-//  */
-// export default Login;
+        <button onClick={login} className="btn btn-success">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
