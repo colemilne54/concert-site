3e8a68ecf89e546abd1c76dc7de2d2f94dba51da
Cole Milne <colemilne54@gmail.com>
2021-12-02 14:26:24 -0700

    final deploy?

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express"
+import path from "path"
+import __dirname from "path"
 import cors from "cors"
 import concerts from "./api/concerts.route.js"
 
@@ -9,5 +11,12 @@ app.use(express.json())
 
 app.use("/api/v1/concerts", concerts)
 app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+
+
+
+// app.use(express.static(path.join(__dirname, '/frontend/build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+// })
 
 export default app
