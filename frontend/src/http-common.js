798d50d6eadea2d46b7843bda3fc0f9a1e602a8f
Cole Milne <colemilne54@gmail.com>
2021-12-03 11:49:48 -0700

    5040 port for project

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/concerts",
+  baseURL: "http://localhost:5040/api/v1/concerts",
   headers: {
     "Content-type": "application/json"
   }
